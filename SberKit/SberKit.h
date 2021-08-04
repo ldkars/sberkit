94e8964cbc5e8806d58f2af50a0d6f143ff2c8b4
Afanasy Koryakin <a.koryakin@okko.tv>
2021-08-04 09:38:43 +0900

    carthage

---
+++ SberKit/SberKit.h
@@ -0,0 +1,18 @@
+//
+//  SberKit.h
+//  SberKit
+//
+//  Created by Афанасий Корякин on 04.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SberKit.
+FOUNDATION_EXPORT double SberKitVersionNumber;
+
+//! Project version string for SberKit.
+FOUNDATION_EXPORT const unsigned char SberKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SberKit/PublicHeader.h>
+
+
---
+++ SberKit/SberbankAPIs.swift
@@ -0,0 +1,14 @@
+//
+//  SberbankAPIs.swift
+//  SberKit
+//
+//  Created by Афанасий Корякин on 04.08.2021.
+//
+
+import Foundation
+
+open class Sberbankapi {
+    func getMillonDollar() -> Int {
+        return 1_000_000
+    }
+}
